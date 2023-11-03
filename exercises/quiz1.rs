e257dfd6df44e876bab65a05ef7cab3b7f0b5f08
mancuoj <1337381768@qq.com>
2023-11-04 00:20:58 +0800

    finish

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(cnt: u32) -> u32 {
+    if cnt > 40 {
+        cnt
+    } else {
+        cnt * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
